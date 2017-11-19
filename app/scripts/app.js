a5271dfb08db06c0498ac0fc7cd1b9edc711477b
David <cdmarchante@gmail.com>
2017-11-19 00:37:39 -0500

    add controller and it's properties

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,5 +8,10 @@
  *
  * Main module of the application.
  */
-angular
-  .module('controllerQuizApp', []);
+angular.module('controllerQuizApp', [])
+    .controller('BrickCtrl', function() {
+        this.color = 'Green';
+        this.size = '2 x 6';
+        this.price = 0.03;
+        this.quantity = 697;
+    });
